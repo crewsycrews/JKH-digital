f203a55e1ec7232ca8a7a565212d71bf2abb6d7a
Danil Rodin <danil.rodin@umbrellait.com>
2020-08-21 21:18:51 +0300

    initial commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7b56eb93d1f591bc882eb10dd173b060'),
+    },
+  },
+});
